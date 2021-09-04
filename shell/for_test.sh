383820f6665c6c2a206c79aec75ce0e541a64676
Iori Sakino <iori.sa1013@icloud.com>
2021-09-04 13:12:00 +0900

    add shell testfiles 0902

---
+++ shell/for_test.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#for文で繰り返し処理をする
+
+end=5
+for ((i=1;i<=end;i++));
+do 
+  echo $i
+done
---
+++ shell/if_for_test.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+#if文とfor文を利用した処理をする
+
+for ((i=1;i<=5;i++));
+do 
+  if [ `expr $i % 2` = 0 ]; then
+    echo "偶数"
+  else
+    echo "奇数"
+  fi
+done
