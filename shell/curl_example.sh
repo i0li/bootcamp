5e2f605c21ce51cff25f87fb0a4b3f155dccd23c
Iori Sakino <iori.sa1013@icloud.com>
2021-09-05 20:33:18 +0900

    add test-files in shell-directory 0905

---
+++ shell/curl_example.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+#大分大学のホームページのファイルをoita.txtに保存
+curl　https://www.oita-u.ac.jp/ > oita-univ.html
---
+++ shell/find_example.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+#ファイルを作成
+touch found_file_1.txt
+
+#作成したファイルを探す
+find found_file_1
+
+#作成していないファイルも探してみる
+find found_file_2
