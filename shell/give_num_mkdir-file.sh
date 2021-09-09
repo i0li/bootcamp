558e95ff8237bf1aa4a20fe991c540ef5cc9300f
Iori Sakino <iori.sa1013@icloud.com>
2021-09-09 22:22:47 +0900

    fix give_num_mkdir-file.sh

--- shell/give_num_mkdir-file.sh
+++ shell/give_num_mkdir-file.sh
@@ -1,10 +1,9 @@
 #!/bin/zsh
 
-
+mkdir out
 for i in $(seq 1 $1); do
-  mkdir dir-$i
+  mkdir out/dir-$i
   for j in $(seq 1 $i); do
-    touch file-$j
-    mv file-$j dir-$i/
+    touch out/dir-$i/file-$j
   done
 done
