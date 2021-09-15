2d6b2d2a74cdc422ad84d8eec17751e479d198b4
iori <iori.sa1013@icloud.com>
2021-09-15 21:06:44 +0900

    add method_return-value_test.rb

---
+++ ruby/method_return-value_test.rb
@@ -0,0 +1,14 @@
+#メソッドの戻り値について
+
+def comparison(num1,num2)
+    if num1 > num2
+        return "#{num1} は #{num2}　以上"
+    elsif num1 < num2
+        return "#{num1} は #{num2}　以下"
+    else
+        return "#{num1} と #{num2}　は等しい"
+    end
+end
+
+
+puts(comparison(5,3))
