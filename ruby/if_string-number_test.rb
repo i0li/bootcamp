5570e48f58e67eae991058a06daed5afcf435d01
iori <iori.sa1013@icloud.com>
2021-09-15 20:51:11 +0900

    add if_string-number_test.rb

---
+++ ruby/if_string-number_test.rb
@@ -0,0 +1,20 @@
+#文字列,数値をif文を用いて比較
+
+#== 文字列 ==
+if "sakino" == "iori"
+    puts("同じ文字列")
+end
+
+if "iori" == "iori"
+    puts("同じ文字列")
+end
+
+
+#== 数値 ==
+if 3 > 1
+    puts("3>1 は　真")
+end
+
+if 1 > 3
+    puts("1>3 は　真")
+end
