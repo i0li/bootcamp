f90292dc62ce28cb762da9e1f9d7f3ece7355243
iori <iori.sa1013@icloud.com>
2021-09-15 20:41:01 +0900

    add puts-print-p_test.rb to ruby/

---
+++ ruby/puts-print-p_test.rb
@@ -0,0 +1,18 @@
+#puts print p の違い
+
+
+#== puts ==
+#自動的に改行をして出力してくれる
+puts("Hello")
+puts("World")
+
+#== print ==
+#出力のみで改行はしない
+print("Hello")
+print("World \n")
+
+#== p == 
+#引数が文字列型の時は"　"をつけて出力
+#引数が数値型の時はそのまま出力
+p("I am")
+p(21)
