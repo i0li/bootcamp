33beaea2c66512fa7a437bfb5c0c9d1f7c861640
iori <iori.sa1013@icloud.com>
2021-09-16 20:27:34 +0900

    add range_test.rb

---
+++ ruby/range_test.rb
@@ -0,0 +1,19 @@
+#Rangeについて
+
+#==== 定義 ===
+numbers = 1..5
+#numbers = Range.new(1, 5)
+
+strings = 'a'..'g'
+#strings = Range.new('a', 'z')
+
+#=== 利用例 ===
+
+#繰り返し処理で使う
+numbers.each do |number|
+    p number
+end
+
+#連続した要素の配列を定義するときに使う(可変長引数として与える)
+array = [*strings]
+p array
