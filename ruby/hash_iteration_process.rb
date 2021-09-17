1fd6d2b4a5947ca8731a73b6fbc29d5de0beb04b
iori <iori.sa1013@icloud.com>
2021-09-17 21:35:08 +0900

    add hash_iteration_process.rb

---
+++ ruby/hash_iteration_process.rb
@@ -0,0 +1,23 @@
+#hashの繰り返し処理
+
+fruits = {"apple" => 200, "grape" => 300}
+p fruits
+
+#== each ==
+#キーとバリューそれぞれをブロック内に渡す
+fruits.each{|fruit_key, fruit_value|
+  puts "#{fruit_key}一つの値段は#{fruit_value}円です"
+}
+
+#== each_key ==
+#キーだけをブロック内に渡して処理を繰り返す
+fruits.each_key{|fruit_key|
+  puts fruit_key
+}
+
+#== each_value ==
+#バリューだけをブロック内に渡して処理を繰り返す
+fruits.each_value{|fruit_value|
+  puts fruit_value
+}
+
