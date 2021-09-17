978ffe91343bdc7d61bec9746b90ea2c6325fea6
iori <iori.sa1013@icloud.com>
2021-09-17 18:43:25 +0900

    add boolean_operator_test.rb

---
+++ ruby/boolean_operator_test.rb
@@ -0,0 +1,32 @@
+#論理演算子まとめ
+
+a = true
+b = false
+puts "a=true , b=false" 
+
+#== ! , not ==
+#aがtreuの時はfalseを、falseの時はtrueを返す
+print "!a → " 
+puts !a 
+
+print "not a → " 
+puts (not a) 
+
+
+#== && , and ==
+#aかつbがtrueの時のみtrueを返す
+print "a && b → " 
+puts a && b 
+
+print "a and b →"
+puts (a and b)
+
+
+
+#== || , or ==
+#aかbがtrue時にtrueを返す
+print "a || b → "
+puts a || b
+print "a or b → "
+print (a or b)
+
