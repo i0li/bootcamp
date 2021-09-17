2e4737743103761a76dc48708e355f9a0ba49abd
iori <iori.sa1013@icloud.com>
2021-09-17 21:13:25 +0900

    fix code and change file_name

--- ruby/break_return_diff.rb
+++ ruby/break_return_diff.rb
@@ -2,13 +2,13 @@
 
 #break : 現在のループを一つ抜ける
 def loop_break
-    3.times do |i|
-        puts("===#{i}")
-        3.times do |j|
-            break if j==1
-            p j
-        end
-    end
+  3.times do |i|
+    puts "===#{i}"
+      3.times do |j|
+        break if j==1
+        p j
+      end
+  end
 end
 
 puts("break")
@@ -17,16 +17,16 @@ loop_break
 
 #return : 現在のメソッド自体から抜ける
 def loop_return
-    3.times do |i|
-        puts("===#{i}")
-        3.times do |j|
-            return if j==1
-            p j
-        end
+  3.times do |i|
+    puts "===#{i}" 
+    3.times do |j|
+      return if j==1
+      p j
     end
+  end
 end
 
-puts("return")
+puts "return" 
 loop_return
 
 #【break】
