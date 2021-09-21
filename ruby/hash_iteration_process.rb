1f3a9625ab40186b36c62634bc141b8e777662ee
iori <iori.sa1013@icloud.com>
2021-09-22 00:21:27 +0900

    fix code

--- ruby/hash_iteration_process.rb
+++ ruby/hash_iteration_process.rb
@@ -5,19 +5,19 @@ p fruits
 
 #== each ==
 #キーとバリューそれぞれをブロック内に渡す
-fruits.each{|fruit_key, fruit_value|
+fruits.each { |fruit_key, fruit_value|
   puts "#{fruit_key}一つの値段は#{fruit_value}円です"
 }
 
 #== each_key ==
 #キーだけをブロック内に渡して処理を繰り返す
-fruits.each_key{|fruit_key|
+fruits.each_key { |fruit_key|
   puts fruit_key
 }
 
 #== each_value ==
 #バリューだけをブロック内に渡して処理を繰り返す
-fruits.each_value{|fruit_value|
+fruits.each_value { |fruit_value|
   puts fruit_value
 }
 
