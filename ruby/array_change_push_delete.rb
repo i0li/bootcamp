4102331d450e722e276164c3f15e31dabfd88524
iori <iori.sa1013@icloud.com>
2021-09-23 03:03:21 +0900

    fix code again

--- ruby/array_change_push_delete.rb
+++ ruby/array_change_push_delete.rb
@@ -1,41 +1,41 @@
 #配列の変更、追加、削除
 
 array = [10, 20, 30, 40, 50]
-p(array)
+p array
 #=== 配列の変更 ===
-puts("---変更")
+puts "---変更"
 #index指定
 array[0] = 0
-p(array)
+p array
 
 #範囲指定
-array[0..1] = [100,200]
-p(array)
+array[0..1] = [100, 200]
+p array
 
 #=== 配列の追加 ===
-puts("---追加")
+puts "---追加"
 array = [10, 20, 30, 40, 50]
 
 #末尾に追加
 array.push(40)
 array << 40
-p(array)
+p array
 
 array.unshift(0)
-p(array)
+p array
 
 #=== 配列の削除 ===
-puts("---削除")
+puts "---削除"
 array = [10, 20, 30, 40, 50]
 
 array.delete(10)
-p(array)
+p array
 
 array.delete_at(0)
-p(array)
+p array
 
 array.shift
-p(array)
+p array
 
 array.slice!(0, 2)
-p(array)
+p array
