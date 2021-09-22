ca82e3bb781e5fe6b1bf49bd77ff6375c5f2f620
iori <iori.sa1013@icloud.com>
2021-09-23 03:26:55 +0900

    fix code again

--- ruby/call_by_value_reference_test.rb
+++ ruby/call_by_value_reference_test.rb
@@ -8,14 +8,14 @@
 #この例であれば、参照渡しの場合はplus_oneメソッドに変数を与えて、その変数に対して
 #変更が加えられると、元の変数も変更される
 #しかし、今回の場合はx,yは変更されていないため値渡しであることがわかる
-def plus_one(a,b)
+def plus_one(a, b)
   a += 1 
   b += 1
 end
 
 x = 10
 y = 20
-plus_one(x,y)
+plus_one(x, y)
 puts x
 puts y
 
@@ -23,18 +23,18 @@ puts y
 #しかし文字列の場合、破壊的メソッドを使った場合参照渡しのような振る舞いをする
 #str2での変更がstr1にほ反映されている
 puts "参照渡し"
-str1 = "hello"
-str2 = str1
-str3 = str2 << " iori"
-puts str1
-puts str2
-puts str3
+string1 = "hello"
+string2 = string1
+string3 = string2 << " iori"
+puts string1
+puts string2
+puts string3
 
 #破壊的メソッドを使わなければ値渡し
 puts "値渡し"
-str1 = "hello"
-str2 = str1
-str3 = str2 + " iori"
-puts str1
-puts str2
-puts str3
+string1 = "hello"
+string2 = string1
+string3 = string2 + " iori"
+puts string1
+puts string2
+puts string3
